1d1151d2ee2824747de740e2a35283b96adf5caf
olehdnd <128152192+ltw30@users.noreply.github.com>
2026-04-22 16:43:19 +0900

    Delete week12/02_거스름돈/taewoong.py

--- week12/02_거스름돈/taewoong.py
+++
@@ -1,9 +0,0 @@
-def solution(n, money):
-    
-    dp = [1] + [0] * (n)
-    
-    for j in money:
-        for i in range(m,n+1):
-            dp[i] += dp[i - j]
-            
-    return dp[n % 1000000007]
